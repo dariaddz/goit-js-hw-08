7e23c7bde2ce902624c1d8b74e2e3173fe1e2613
dariaddz <dariadz@gmail.com>
2021-11-20 17:01:30 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import throttle from 'lodash/throttle';
+import Player from '@vimeo/player';
 
+const playerIframe = document.querySelector('#vimeo-player');
+const player = new Player(playerIframe);
+
+// --------play video-----------
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+// -------------get current time - throttled----------
+player.on('timeupdate', throttle(currentTime, 1000));
+function currentTime(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  console.log('current time: ', data.seconds);
+}
+
+// ---------set start time if saved------------
+const timeCatcher = localStorage.getItem('videoplayer-current-time');
+
+if (timeCatcher) {
+  player.setCurrentTime(timeCatcher);
+  localStorage.removeItem('videoplayer-current-time');
+}
+console.log('timeCatcher: ', timeCatcher);
