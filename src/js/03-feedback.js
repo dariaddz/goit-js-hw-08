7a2a26ffc39f994bfca8194d68452a27126eccc0
dariaddz <dariadz@gmail.com>
2021-11-20 20:47:21 +0200

    task 3 -halfway

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,66 @@
+import throttle from 'lodash/throttle';
 
+// const form = document.querySelector('form');
+const emailInput = document.querySelector('input[name=email]');
+const messageInput = document.querySelector('textarea[name=message]');
+const btnSubmit = document.querySelector('button[type=submit]');
+
+// ------container for local storage-----------
+const userInfo = {
+  email: '',
+  message: '',
+};
+
+const getLocalStorage = localStorage.getItem('feedback-form-state');
+
+// ----------event listeners--------
+emailInput.addEventListener('input', throttle(onEmailType, 500));
+messageInput.addEventListener('input', throttle(onMessageType, 500));
+btnSubmit.addEventListener('click', onButtonSubmit);
+
+filledForm();
+// -------------local storege data save---------
+function onEmailType() {
+  userInfo.email = emailInput.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(userInfo));
+  console.log(userInfo);
+}
+
+function onMessageType() {
+  userInfo.message = messageInput.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(userInfo));
+  console.log(userInfo);
+}
+
+// ----------submit button-----------
+//делаем переменную для хранения отправленных данных,
+// записываем в нее данные из хранилища по нажатию кнопки,
+//очищаем хранилище и форму
+
+function onButtonSubmit(e) {
+  e.preventDefault();
+  const userInfoSubmited = JSON.parse(getLocalStorage);
+  console.log('user info submited: ', userInfoSubmited);
+
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+}
+
+// ----------local storage check if saved ----------
+
+function filledForm() {
+  if (getLocalStorage) {
+    emailInput.value = JSON.parse(getLocalStorage).email;
+    messageInput.value = JSON.parse(getLocalStorage).message;
+    console.log(
+      `в хранилище осталось email = ${JSON.parse(getLocalStorage).email} и message = ${
+        JSON.parse(getLocalStorage).message
+      }`,
+    );
+  }
+}
+
+// function filledForm() {
+//   console.log('feedback-form-state');
+// }
